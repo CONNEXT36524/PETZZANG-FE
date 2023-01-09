9bb55e4a8badd2567d79cf07cf262b138dd597d1
Dajeong Park <jul897813@gmail.com>
2023-01-09 10:27:58 +0900

    fix: resolve react-bootstrap.css application

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-
-import './App.css';
-import {Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import {Container, Nav, Navbar} from 'react-bootstrap';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css"; //bootstrap css 적용
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import Home from "./pages/Home";
 
 function App() {
-  return (
-    <Router>
-    <Navbar collapseOnSelect fixed="top" className="menu" bg="white">
-      <Container>
-        <Navbar.Brand href="/" className='Navbar-logo'> 사이트</Navbar.Brand>
-          {/* <Nav.Link className = "item" href="/about">소개</Nav.Link> */}
-          <div className='item1'>
-          <Nav.Link href="/">그거</Nav.Link>
-          </div>
-            <div className='item2'>
-            <Nav.Link href="/">로그인</Nav.Link> 
-          </div>
-      </Container>
-    </Navbar>
+	return (
+		<Router>
+			<Navbar collapseOnSelect fixed="top" className="menu" bg="white">
+				<Container>
+					<Navbar.Brand href="/" className="Navbar-logo">
+						{" "}
+						사이트
+					</Navbar.Brand>
+					{/* <Nav.Link className = "item" href="/about">소개</Nav.Link> */}
+					<div className="item1">
+						<Nav.Link href="/">그거</Nav.Link>
+					</div>
+					<div className="item2">
+						<Nav.Link href="/">로그인</Nav.Link>
+					</div>
+				</Container>
+			</Navbar>
 
-    <main className='main'>
-      {/* Switch has been replaced with Routes from v6 */}
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        {/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
-      </Routes>
-    </main>
-  </Router>
-  );
+			<main className="main">
+				{/* Switch has been replaced with Routes from v6 */}
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					{/* <Route path="*" element={<NotFound />} /> */}
+					{/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
+				</Routes>
+			</main>
+		</Router>
+	);
 }
 
 export default App;
