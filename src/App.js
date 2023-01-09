d685fdf8da36548c4ae52e552823dff26019b4f8
HM <climatc1103@naver.com>
2023-01-09 09:16:03 +0900

    fix main

--- src/App.js
+++ src/App.js
@@ -7,28 +7,26 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
-    <Navbar collapseOnSelect fixed="top" className="menu" bg="white">
-      <Container>
-        <Navbar.Brand href="/" className='Navbar-logo'> 사이트</Navbar.Brand>
-          {/* <Nav.Link className = "item" href="/about">소개</Nav.Link> */}
-          <div className='item1'>
-          <Nav.Link href="/">그거</Nav.Link>
-          </div>
+      <Navbar collapseOnSelect fixed="top" className="menu" bg="white">
+        <Container>
+          <Navbar.Brand href="/" className='Navbar-logo'> 사이트</Navbar.Brand>
+            <div className='item1'>
+            <Nav.Link href="/">그거</Nav.Link>
+            </div>
             <div className='item2'>
             <Nav.Link href="/">로그인</Nav.Link> 
-          </div>
-      </Container>
-    </Navbar>
+            </div>
+        </Container>
+      </Navbar>
 
-    <main className='main'>
-      {/* Switch has been replaced with Routes from v6 */}
+      <main className='main'>
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
-        {/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
-      </Routes>
-    </main>
-  </Router>
+          <Route exact path="/" element={<Home />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+          {/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
