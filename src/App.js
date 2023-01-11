9b03e7d4e9a1908f0b3bd91ec2ef6f86c8789dcf
root <80766477+cholong2@users.noreply.github.com>
2023-01-11 10:46:22 +0900

    feat: Add login api

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import {Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import {Container, Nav, Navbar} from 'react-bootstrap';
 import Home from "./pages/Home";
+import KakaoLogin from "./pages/Login/KakaoLogin";
 
 function App() {
+  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+  const rest_api_key = process.env.REACT_APP_REST_API_KEY;
+  const KAKAO_AUTH_URL = process.env.REACT_APP_KAKAO_AUTH_URL
   return (
     <Router>
       <Navbar collapseOnSelect fixed="top" className="menu" bg="white">
@@ -14,7 +18,8 @@ function App() {
             <Nav.Link href="/">그거</Nav.Link>
             </div>
             <div className='item2'>
-            <Nav.Link href="/">로그인</Nav.Link> 
+            <Nav.Link className='user-logo' href={KAKAO_AUTH_URL}></Nav.Link> 
+            
             </div>
         </Container>
       </Navbar>
@@ -22,6 +27,7 @@ function App() {
       <main className='main'>
       <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route path="/oauth/callback/kakao" element={<KakaoLogin />}/>
           {/* <Route path="*" element={<NotFound />} /> */}
           {/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
         </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
 const Home=()=>{
+    const [hello, sethello] = useState('')
+
+    useEffect(()=> {
+        axios.get('/api/hello')
+        .then(response => sethello(response.data))
+        .catch(error=> console.log(error))
+    }, []);
+
     return(
         <div>
-            hi
+            연동성공 ? : {hello}
         </div>
         
 
