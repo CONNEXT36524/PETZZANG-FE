fe2be46548ca81099e1d33fe3cdc0347ad2de4be
HM <climatc1103@naver.com>
2023-01-11 11:34:34 +0900

    fix home

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import Home from "./pages/Home/Home";
 import Ranking from "./pages/ranking/Ranking";
 import Posting from "./pages/Community/Posting/Posting";
-import KakaoLogin from "./pages/Login/KakaoLogin"
 import { useSelector} from "react-redux/";
 import Daily from "./pages/Community/Board/Daily";
 import KakaoLogin from "./pages/Login/KakaoLogin";
