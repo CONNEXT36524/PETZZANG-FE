df00fc5b9b6b33e0f98ad010ba7cdd6e0eef9db0
Dajeong Park <jul897813@gmail.com>
2023-01-11 15:06:01 +0900

    style: Change logo image

--- src/App.js
+++ src/App.js
@@ -9,17 +9,15 @@ import Daily from "./pages/Community/Board/Daily";
 import Search from "./pages/Community/Board/Search";
 import KakaoLogin from "./pages/Login/KakaoLogin";
 
-
 import "bootstrap/dist/css/bootstrap.css"; //bootstrap css 적용
 
 function App() {
-  const pagetype= useSelector((state)=>state.Nav.pagetype);
-  const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
-  const rest_api_key = process.env.REACT_APP_REST_API_KEY;
-  const KAKAO_AUTH_URL = process.env.REACT_APP_KAKAO_AUTH_URL;
+	const pagetype = useSelector((state) => state.Nav.pagetype);
+	const redirect_uri = process.env.REACT_APP_REDIRECT_URI;
+	const rest_api_key = process.env.REACT_APP_REST_API_KEY;
+	const KAKAO_AUTH_URL = process.env.REACT_APP_KAKAO_AUTH_URL;
 
 	return (
-
 		<Router>
 			<Navbar collapseOnSelect fixed="top" className="menu" id={pagetype}>
 				<Container>
@@ -37,25 +35,35 @@ function App() {
 					<div className="item2">
 						<Nav.Link className='user-logo' href={KAKAO_AUTH_URL}></Nav.Link>
 					</div> */}
-					<NavDropdown
-						title="커뮤니티"
-						id={`communityDropdown`}>
-						
-						<NavDropdown.Item href="/community/daily">일상</NavDropdown.Item>
-						<NavDropdown.Item href="/community/boast">자랑</NavDropdown.Item>
-						<NavDropdown.Item href="/community/question">질문</NavDropdown.Item>
-						<NavDropdown.Item href="/community/recommendation">제품 추천</NavDropdown.Item>
+					<NavDropdown title="커뮤니티" id={`communityDropdown`}>
+						<NavDropdown.Item href="/community/daily">
+							일상
+						</NavDropdown.Item>
+						<NavDropdown.Item href="/community/boast">
+							자랑
+						</NavDropdown.Item>
+						<NavDropdown.Item href="/community/question">
+							질문
+						</NavDropdown.Item>
+						<NavDropdown.Item href="/community/recommendation">
+							제품 추천
+						</NavDropdown.Item>
 					</NavDropdown>
 
-					<NavDropdown
-						title="랭킹"
-						id={`rankingDropdown`}>
-						<NavDropdown.Item href="/Ranking">주간 랭킹</NavDropdown.Item>
-						<NavDropdown.Item href="/Ranking">월간 랭킹</NavDropdown.Item>
+					<NavDropdown title="랭킹" id={`rankingDropdown`}>
+						<NavDropdown.Item href="/Ranking">
+							주간 랭킹
+						</NavDropdown.Item>
+						<NavDropdown.Item href="/Ranking">
+							월간 랭킹
+						</NavDropdown.Item>
 					</NavDropdown>
 
 					<div className="user">
-						<Nav.Link className='user-logo' href={KAKAO_AUTH_URL}></Nav.Link>
+						<Nav.Link
+							className="user-logo"
+							href={KAKAO_AUTH_URL}
+						></Nav.Link>
 					</div>
 				</Container>
 			</Navbar>
@@ -67,8 +75,15 @@ function App() {
 					<Route path="/community/posting" element={<Posting />} />
 					<Route exact path="/Ranking" element={<Ranking />} />
 					<Route exact path="/community/daily" element={<Daily />} />
-					<Route exact path="/community/search" element={<Search />} />
-					<Route path="/oauth/callback/kakao" element={<KakaoLogin />}/>
+					<Route
+						exact
+						path="/community/search"
+						element={<Search />}
+					/>
+					<Route
+						path="/oauth/callback/kakao"
+						element={<KakaoLogin />}
+					/>
 
 					{/* <Route path="*" element={<NotFound />} /> */}
 					{/* 지정하지 않은 주소로 들어올 때는 NotFound가 뜬다. */}
@@ -76,7 +91,6 @@ function App() {
 			</main>
 		</Router>
 	);
-
 }
 
 export default App;
