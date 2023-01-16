9a7d0bc3ec19cfdd5b88d9c1109c3a080cb8c9da
Dajeong Park <jul897813@gmail.com>
2023-01-16 12:55:35 +0900

    feat: Create Posts Page

---
+++ src/pages/Community/Posts/Posts.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./Posts.css";
+import postImg from "../../../assets/maltese1.png";
+import CommunityBanner from "../../../components/banner/CommunityBanner";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Container from "react-bootstrap/Container";
+import MiddleNav from "../../../components/navbar/MNB/MiddleNav";
+import SavePostingModal from "../../../components/modal/SavePostingModal";
+function Posts(props) {
+	//Editor
+	const [desc, setDesc] = useState("");
+	function onEditorChange(value) {
+		setDesc(value);
+	}
+
+	//Modal
+	const [modalShow, setModalShow] = React.useState(false);
+
+	//MNB 정보
+	//const location = useLocation();
+	const content = "HOME>커뮤니티>일상";
+
+	return (
+		<div>
+			<CommunityBanner />
+
+			<MiddleNav contents={content} />
+
+			<Container className="articles">
+				<br />
+
+				<div className="articleHeaderTop">
+					<div className="aht-section1"></div>
+					<div className="aht-section2">
+						<h1 className="aht-title">
+							인형인가 말티즈인가우리 아이 너무 인형처럼 생기지
+							않았나요?!나요?!
+							나요?!나요?!나요?!나요?!나요?!이름은 소금이에요!
+						</h1>
+					</div>
+					<div className="aht-section3">
+						<h6 className="aht-viewNum">조회수 29</h6>
+					</div>
+				</div>
+				<hr size="0" />
+				<div className="articleHeaderBottom">
+					<h6>소금엄마 | 2022.01.04 16:08:29</h6>
+				</div>
+				<br />
+				<div className="articleBody">
+					<img className="postImg" src={postImg}></img>
+					<p>
+						우리 아이 너무 인형처럼 생기지 않았나요?! <br />
+						이름은 소금이에요!
+					</p>
+				</div>
+			</Container>
+
+			<Container className="comments">
+				<h5>❤️ 2 💭 0</h5>
+				<div>comments 공간</div>
+				<div className="writeCommentBox">
+					<div contentEditable="true" className="writeCommentContent">
+						댓글을 남겨주세요.
+					</div>
+					<Button variant="warning" className="writeCommentBtn">
+						작성
+					</Button>
+
+					<InputGroup className="mb-3">
+						<Form.Control
+							placeholder="댓글을 남겨주세요."
+							aria-label="댓글"
+							aria-describedby="writer"
+						/>
+						<Button variant="outline-secondary" id="button-addon2">
+							작성
+						</Button>
+					</InputGroup>
+				</div>
+			</Container>
+		</div>
+	);
+}
+
+export default Posts;
