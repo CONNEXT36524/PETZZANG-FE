b0e94557402ae7418904668717c8dc2149c23913
cholong2 <pcl106@naver.com>
2023-01-27 11:35:13 +0900

    feat: Add profile image change

--- src/pages/Mypage/Account.jsx
+++ src/pages/Mypage/Account.jsx
@@ -3,14 +3,15 @@ import "./Mypage.css"
 import { useDispatch} from "react-redux/";
 import { changepagetype } from "../../Slice/Navslice";
 import { useEffect } from "react";
+import { useState, useRef } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import MypageBanner from "../../components/banner/MypageBanner";
 import Sidebar from "../../components/mypage/Sidebar";
 import styled from "styled-components";
-
-import { Toggle } from "../../components/mypage/Toggle"
+import myImage from "../../assets/default_profile.png";
+import DeleteAccountModal from "../../components/modal/DeleteAccountModal";
 
 const Center = styled.div`
   height: 92vh;
@@ -34,8 +35,14 @@ const Sub =styled.div`
     height: 60vh;
     background-color: rgb(255, 255, 255);
     border: 1px solid rgb(234, 234, 234);
-    flex-direction: row;
+    display : flex;
 `
+
+//실제 이미지는 Storage에 저장하고, 주소를 활용해 처리한다!
+// storage는 파일 영상등 저장하는 db 
+// 1. input 태그를 통해 파일 선택 후 파일 객체 들어온 걸 백엔드에 이미지 업로드 요청 
+// 2. storage에서는 파일을 저장 그 파일을 다운받을 수 있는 주소를 줌 
+// 3. 백엔드가 그 주소를 받아서 다시 프론트로 전달 
 
 const Account=()=>{
     
@@ -43,7 +50,36 @@ const Account=()=>{
     useEffect(()=>{
         dispatch(changepagetype("mypage"))
     },[dispatch])
+
+
+	const [modalShow, setModalShow] = useState(false);
+    const [Image, setImage] = useState(myImage)
+    const profileInputRef = useRef();
+ 
+    const uploadImageBtnClick = (event) =>{
+        event.preventDefault();
+        profileInputRef.current.click();
+    }
+
+    const uploadImageChange = async (e) =>{
+        const file = profileInputRef.current.files[0];
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+        setImage(reader.result);
+    }
+    }
+
+    // 중복확인 
+    const duplicateCheck = () => {
+
+    }
+
+    // 회원탈퇴
+    const deleteAccount = () => {
+    }
     
+
     return(
         <>
             <MypageBanner/>
@@ -68,16 +104,33 @@ const Account=()=>{
                   <Title>
                         <div className="title">나의 계정</div>
                   </Title>
-                  <Sub>
-            
-                        
+                    <Sub>
+                    <div className = "circle" 
+                    onClick={()=>{profileInputRef.current.click()}}>
+                    <img className="profile" src={Image ? Image : {myImage}}/>
+                    <input style={{ display: "none" }} type="file" accept="image/*" className="profileInput" ref={profileInputRef} onChange={uploadImageChange} />
+                    </div>
+                    <div className="modify">
+                        <div>프로필 사진 변경</div>
+                        <div className="nickname">
+                            <div>닉네임</div>
+                            <input type="text" />
+                            <button className="duplicateBtn" onClick={duplicateCheck}> 중복확인 </button>
+                        </div>
+                        <button onClick={() => setModalShow(true)}>회원탈퇴</button>
+                        <DeleteAccountModal 
+                            show={modalShow}
+                            onHide={() => setModalShow(false)}
+                            />
+                        <button className="saveBtn" onClick={uploadImageBtnClick}>변경사항 저장</button>
+                    </div>
                   </Sub>
                   
                 </Content>
             </Center>
-      
             
         </>
     );
 }
+
 export default Account;
