29f7e0e20949b5ea57d7e867791fe2049d35eab9
Dajeong Park <jul897813@gmail.com>
2023-02-14 09:29:13 +0900

    test: upload image to object storage

--- src/service/PostingService.jsx
+++ src/service/PostingService.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const COMMUNITY_API_BASE_URL = "http://localhost:8080/api/community";
-
+const token =
+	"gAAAAABj5fHxBhZ-39uXunxm_77tDYNf7nS1gDGbp9Bo-SU41wUi-UsEHpjC997jmYxnAkx9PytaQRBcIlkho9WoPYSykwHi7F0_laOwbQLuCjK7iQd2kZqWiRFiifrBw1EBJzwa1K1W0wzluPUrbIB7BtRxrUy79xlxx0q3b17SQ-Q8_NV9B4bPN9exMcRjX9qv-Sw41fwT";
 class PostingService {
 	//게시글 불러오기
 	getPosts() {
@@ -17,6 +18,23 @@ class PostingService {
 			},
 		});
 	}
+
+	uploadThumbnail(img) {
+		return axios.put(COMMUNITY_API_BASE_URL + "/image", img, {
+			headers: {
+				"Content-Type": `multipart/form-data; `,
+			},
+		});
+	}
+
+	// uploadThumbnail(img) {
+	// 	return axios.put(COMMUNITY_API_BASE_URL + "/image", img, {
+	// 		headers: {
+	// 			"X-AUTH-TOKEN": token,
+	// 			"Content-Type": `multipart/form-data`,
+	// 		},
+	// 	});
+	// }
 }
 
 export default new PostingService();
